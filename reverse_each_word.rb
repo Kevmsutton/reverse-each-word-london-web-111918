6ef7443727edbaa83ea7b0f9bfc28c8a14769034
Kevin Sutton <kevmsutton@gmail.com>
2018-11-13 10:26:12 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,8 @@
+reverse_w = []
 def reverse_each_word(string)
 	newString = string.split
 	newString.each do |word|
-  return puts word.reverse
+  reverse_w.push (word.reverse)
 end
+return reverse_w
 end
