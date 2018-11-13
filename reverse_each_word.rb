5bd2acf15bb4a346712dfce02e66ced0e92a3002
Kevin Sutton <kevmsutton@gmail.com>
2018-11-13 10:49:21 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,9 @@ def reverse_each_word(string)
 	extra_string = ""
 	newString = string.split(" ")
 	newString.each do |word|
-  return word.reverse
+  extra_string = word.reverse
 end
+return extra_string
 end
 
 reverse_each_word("Hello, how are you")
